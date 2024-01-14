62856a77d3d15a4f9dbf9cfb795a6a64bcc9773e
Sumitsahani <sumitsahani.1855@gmail.com>
2024-01-14 16:02:18 +0530

    update

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [ "Developer",  "Designer"],
+        strings:[ "Web Developer","Freelancer","App Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: [ "Developer","Designer"],
+        strings: [ "Web Developer","Freelancer","App Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
